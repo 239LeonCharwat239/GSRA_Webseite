eb0217620ac7a5686f2e4e685a0be5331fe8f4a1
239LeonCharwat239 <Leon.Charwat@Outlook.de>
2026-08-31 20:03:14 +0200

    Update supabase-client.js

--- supabase-client.js
+++ supabase-client.js
@@ -1,5 +1,5 @@
 // Supabase Initialisierung
-const SUPABASE_URL = 'https://mqqyvpnddaastacldbyy.supabase.co'; // Hier deine Supabase URL eintragen
+const SUPABASE_URL = 'mqqyvpmddaastacldbyy'; // Hier deine Supabase URL eintragen
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1xcXl2cG1kZGFhc3RhY2xkYnl5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc0MzAzNzUsImV4cCI6MjEwMzAwNjM3NX0.OWY2FUS7n9Jw-6FP0Hp1B9aY6AUAkEpZ6Xd7MpS0GLI'; // Hier deinen Supabase Anon Key eintragen
 
 // Erstellt die Supabase-Instanz nur, wenn sie nicht schon existiert
